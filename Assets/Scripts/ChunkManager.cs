6b431c4181bbb8ae9ff94dc98c8e75cfb441bb4e
Luke <luke4.roberts@live.uwe.ac.uk>
2018-11-20 17:06:26 +0000

    initial project upload

---
+++ Assets/Scripts/ChunkManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class ChunkManager : MonoBehaviour
+{
+    public GameObject stone;
+
+    void Start ()
+    {
+        //GameObject stone = (GameObject)AssetDatabase.LoadAssetAtPath(("Assets/Prefabs/Stone"), typeof(GameObject));
+
+        int i = 0;
+
+		foreach (Transform block in transform)
+        {
+            string path = "Assets/StreamingAssets/0";
+            
+            Instantiate(stone, block.position, Quaternion.identity);
+
+            i++;
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 0.0f;
+    public float rotateSpeed = 0.0f;
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if (Input.GetKey("w"))
+        {
+            transform.position += (transform.forward * (moveSpeed * 0.01f)) ;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            transform.position += -(transform.forward * (moveSpeed * 0.01f));
+        }
+
+        if (Input.GetKey("a"))
+        {
+            transform.position += -(transform.right * (moveSpeed * 0.01f));
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.position += (transform.right * (moveSpeed * 0.01f));
+        }
+
+        if (Input.GetKey("q"))
+        {
+            transform.Rotate(-(transform.up * (Time.deltaTime * rotateSpeed)));
+        }
+
+        if (Input.GetKey("e"))
+        {
+            transform.Rotate(transform.up * (Time.deltaTime * rotateSpeed));
+        }
+    }
+}
